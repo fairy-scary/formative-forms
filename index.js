4bbc7a59236a770977871d2bc0c8e76c55496052
Barry Mattern <bdmattern@gmail.com>
2020-11-18 14:15:33 -0700

    Phase 2, all but mixins

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
-
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const csrfProtection = csrf({ cookie: true });
 const app = express();
+
+
+
+app.use(cookieParser());
+
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render('index');
+});
+
+app.get('/create', csrfProtection, (req, res, next) => {
+  res.render('create', { title: 'Create User', csrfToken: req.csrfToken() });
 });
 
 const users = [
